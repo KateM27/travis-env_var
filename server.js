08587473e4a4250a1bd0bb842d0ca4251592b853
KateM27 <mamocatherine@gmail.com>
2022-09-10 14:25:53 +0300

    setting env variables

---
+++ server.js
@@ -0,0 +1,14 @@
+const FAVORITE_CLOUD_PROVIDER = process.env.NAME;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log("${FAVORITE_CLOUD_PROVIDER} rocks!");
+    await sleep(5000);
+  }
+}
+
+main();
